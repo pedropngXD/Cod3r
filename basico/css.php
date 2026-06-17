9359388aace375f540bb14455a53c66b5784d7c3
pedropngXD <pgpmoser@gmail.com>
2026-06-16 21:59:05 -0300

    Interagindo entre CSS e PHP

---
+++ basico/css.php
@@ -0,0 +1,42 @@
+<div class="titulo">Integração CSS</div>
+
+<h1 center>
+<?php
+echo 'Olá';
+echo '<small>';
+echo ' Mundo';
+echo '<small>';
+?>
+</h1>
+
+<?= "<div center azul>Outra forma de me 'expressar'!</div>" ?>
+
+<br>
+<div center>
+    <button dobro>
+        <?= "Botão legal" ?>
+    </button>
+</div>
+
+<style>
+    button {
+        padding: 5px <?= 2 * 10 ?>px;
+        background-color: #4286f4;
+        color: #EEE;
+        font-weight: bold;
+        border-radius: <?= 5+5 ?>px;
+    }
+
+    [center] {
+        display: flex;
+        justify-content: center;
+    }
+
+    <?= "[azul]" ?> {
+        color: #4286f4;
+    }
+
+    [dobro] {
+        font-size: <?= 10-8 ?>rem;
+    }
+</style>
