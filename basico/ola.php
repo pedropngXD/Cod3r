24d690b16755254e37c2805c17dca44454b055f1
pedropngXD <pgpmoser@gmail.com>
2026-06-16 21:40:07 -0300

    Tags PHP, função echo e phpinfo

---
+++ basico/ola.php
@@ -0,0 +1,14 @@
+<div class="titulo">Olá PHP</div>
+
+<h2>Exemplo A</h2>
+<?php 
+echo 'Olá <br>';
+echo 'Mundo!';
+?>
+
+<h2>Exemplo B</h2>
+<?= "Outra forma de me 'expressar'!"?>
+
+<h2>Função phpinfo()</h2>
+<?php
+phpinfo();
--- index.php
+++ index.php
@@ -20,7 +20,11 @@
                 <div class="modulo verde">
                     <h3>Modulo 01</h3>
                     <ul>
-                        <li><a href="exercicio.php?dir=teste&file=teste">Exercicio A</a></li>
+                        <li>
+                            <a href="exercicio.php?dir=basico&file=ola">
+                                Olá PHP
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </nav>
