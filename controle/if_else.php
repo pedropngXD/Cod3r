6e951fe2024fbe70ae7b6200b1f9c60478ae5234
pedropngXD <pgpmoser@gmail.com>
2026-06-18 23:03:31 -0300

    + If / Else

--- controle/if_else.php
+++ controle/if_else.php
@@ -25,9 +25,26 @@ echo "<br>";
 
 //------------------------------//------------------------------//
 
-//cuidado com ponto e virgula ( ;)
+//cuidado com ponto e virgula ()
 if(false); {
-    echo "Serei impresso novamente?";
+    echo "Serei impresso novamente (2)?";
     echo '<br>';
-    echo "Serei impresso mais uma vez?";
+    echo "Serei impresso mais uma vez (2)?";
+}
+
+echo "<br>";
+
+//cuidado com os parametros de true e false
+if("") {
+    echo "Serei impresso novamente (3)?";
+    echo '<br>';
+    echo "Serei impresso mais uma vez (3)?";
+}
+
+echo "<br>";
+
+if(" ") {
+    echo "Serei impresso novamente (4)?";
+    echo '<br>';
+    echo "Serei impresso mais uma vez (4)?";
 }
