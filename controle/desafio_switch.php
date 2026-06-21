38d405c2eaf3dba344363dedee885db3d616692d
pedropngXD <pgpmoser@gmail.com>
2026-06-21 17:17:37 -0300

    Desafio Switch 2

--- controle/desafio_switch.php
+++ controle/desafio_switch.php
@@ -7,6 +7,8 @@
         <option value="milha-km">Milha > Km</option>
         <option value="km-metro">Km > Metro</option>
         <option value="metro-km">Metro > Km</option>
+        <option value="c-f">Celsius > Fahrenheit</option>
+        <option value="f-c">Fahrenheit > Celsius</option>
     </select>
     <button>Calcular</button>
 </form>
@@ -23,27 +25,38 @@
 
 const fatorKmMilha = 0.621371;
 const fatorMetroKm = 1000;
+const fatorCelFah = 1.8;
 
 $param = $_POST['param'] ?? 0;
 $conversao = $_POST['conversao'];
 
 switch ($conversao) {
     case 'km-milha':
-        $distancia = $param / fatorKmMilha;
-        $mensagem = "$param Km(s) = $distancia Milha(s)";
+        $valor = $param / fatorKmMilha;
+        $mensagem = "$param Km(s) = $valor Milha(s)";
         break;
     case 'milha-km':
-        $distancia = $param / fatorKmMilha;
-        $mensagem = "$param Milha(s) = $distancia Km(s)";
+        $valor = $param / fatorKmMilha;
+        $mensagem = "$param Milha(s) = $valor Km(s)";
         break;
     case 'km-metro' :
-        $distancia = $param / fatorMetroKm;
-        $mensagem = "$param Km(s) = $distancia Metro(s)";
+        $valor = $param / fatorMetroKm;
+        $mensagem = "$param Km(s) = $valor Metro(s)";
         break;
     case 'metro-km':
-        $distancia = $param / fatorMetroKm;
-        $mensagem = "$param Metro(s) = $distancia Km(s)";
+        $valor = $param / fatorMetroKm;
+        $mensagem = "$param Metro(s) = $valor Km(s)";
+        break;
+    case 'c-f':
+        $valor = $param * fatorCelFah + 32;
+        $mensagem = "{$param}° Celsius = {$valor}° Fahrenheit";
+        break;
+    case 'f-c':
+        $valor = ($param - 32) / fatorCelFah;
+        $mensagem = "{$param}° Fahrenheit = {$valor}° Celsius";
         break;
     default :
         $mensagem = "Nenhum valor calculado!";
-} echo "<p>$mensagem</p>";
+}
+
+echo "<p>$mensagem</p>";
