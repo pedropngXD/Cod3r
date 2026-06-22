51ecef52a163963cd1824aa9b23a237c30e319dd
pedropngXD <pgpmoser@gmail.com>
2026-06-22 20:47:59 -0300

    Desafio Meses

---
+++ array/desafio_meses.php
@@ -0,0 +1,47 @@
+<div class="titulo">Desafio Meses</div>
+
+<?php
+
+//minha resposta:
+
+$meses = array(
+    1 => "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+);
+
+print_r($meses);
+echo '<br>' . $meses[5];
+
+//-------------------//
+echo '<br>';
+//-------------------//
+
+//resposta do professor:
+
+$meses = array(
+    1 => "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+);
+
+print_r($meses);
+echo '<br>' . $meses[12];
