843a30f4808e2d1d0e9c4525e9c3da24bdacf212
pedropngXD <pgpmoser@gmail.com>
2026-07-12 15:41:24 -0300

    Laço For

--- index.php
+++ index.php
@@ -19,6 +19,17 @@
         <div class="conteudo">
             <nav class="modulos">
 
+                <div class="modulo verde-escuro">
+                    <h3>6. Repetições</h3>
+                    <ul>
+                        <li>
+                            <a href="exercicio.php?dir=repeticoes&file=for">
+                                Laço For
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
                 <div class="modulo rosa">
                     <h3>5. Array</h3>
                     <ul>
---
+++ repeticoes/for.php
@@ -0,0 +1,53 @@
+<div class="titulo">Laço For</div>
+
+<?php
+
+for($cont = 1; $cont <= 5; $cont++) {
+    echo "$cont <br>";
+}
+
+echo '<hr>';
+
+for (; $cont <= 10; $cont++) {
+    echo "$cont <br>";
+}
+
+echo '<hr>';
+
+for (; $cont <= 15;) {
+    echo "$cont <br>";
+    $cont++;
+}
+
+echo '<hr>';
+
+$array = [
+    1 => 'Domingo', 
+    'Segunda', 
+    'Terca', 
+    'Quarta', 
+    'Quinta', 
+    'Sexta', 
+    'Sábado'
+];
+
+print_r($array);
+echo '<hr>';
+
+for ($i = 1; $i <= count($array); $i++) {
+    echo "{$array[$i]} <br>";
+}
+
+echo '<hr>';
+
+$matrix = [
+    ['a', 'e', 'i', 'o', 'u'],
+    ['b', 'c', 'd']
+];
+
+for ($i = 0; $i < count($matrix); $i++) {
+    for ($j = 0; $j < count($matrix[$i]); $j++) {
+        echo "{$matrix[$i][$j]}";
+    }
+    echo "<br";
+};
